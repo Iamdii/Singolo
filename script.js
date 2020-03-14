1679a1e782e16cd80113041ef5a886ce5cb0a897
Di <azazarova99@mail.ru>
2020-03-15 00:53:31 +0300

    feat: add on/off mode to screens

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function navBarCleaner() {
 function selectNavBarItem(itemId) {
     document.getElementById(itemId).classList.add('selected-navbar-item');
 }
+
+
+
+document.querySelector(".phone-horizontal").addEventListener('click', function() {
+    hideDisplay(document.querySelector(".screen-h"));
+});
+
+document.querySelector(".phone-vertical").addEventListener('click', function() {
+    hideDisplay(document.querySelector(".screen-v"));
+});
+
+const hideClass = 'hide-display';
+
+function hideDisplay(element) {
+    if(element.classList.contains(hideClass)) {
+        element.classList.remove(hideClass);
+    } else {
+        element.classList.add(hideClass);
+    }
+}
+
+
+
