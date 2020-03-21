73258a3095969284e951595fe01a17e45ab6be3b
Di <azazarova99@mail.ru>
2020-03-21 18:28:42 +0300

    fix: edit slider section

--- script.js
+++ script.js
@@ -18,6 +18,57 @@ document.addEventListener('scroll', function() {
     })
 });
 
+/* Карусель */
+
+let items = document.querySelectorAll('.item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('active', direction);
+    });
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    });
+}
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
+    showItem('from-left');
+}
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1);
+    showItem('from-right');
+}
+
+document.querySelector('.left-arrow').addEventListener('click', function() {
+    if(isEnabled) {
+        previousItem(currentItem);
+    }
+})
+
+document.querySelector('.right-arrow').addEventListener('click', function() {
+    if(isEnabled) {
+        nextItem(currentItem);
+    }
+})
+
 /* Активация экранов телефонов */
 
 document.querySelector(".phone-horizontal").addEventListener('click', function() {
@@ -139,54 +190,3 @@ function submit() {
 
     document.querySelector('.details-description').innerHTML = inputDetails;
 }
-
-/* Карусель */
-
-let items = document.querySelectorAll('.item');
-let currentItem = 0;
-let isEnabled = true;
-
-function changeCurrentItem(n) {
-    currentItem = (n + items.length) % items.length;
-}
-
-function hideItem(direction) {
-    isEnabled = false;
-    items[currentItem].classList.add(direction);
-    items[currentItem].addEventListener('animationend', function() {
-        this.classList.remove('active', direction);
-    });
-}
-
-function showItem(direction) {
-    items[currentItem].classList.add('next', direction);
-    items[currentItem].addEventListener('animationend', function() {
-        this.classList.remove('next', direction);
-        this.classList.add('active');
-        isEnabled = true;
-    });
-}
-
-function previousItem(n) {
-    hideItem('to-right');
-    changeCurrentItem(n - 1);
-    showItem('from-left');
-}
-
-function nextItem(n) {
-    hideItem('to-left');
-    changeCurrentItem(n + 1);
-    showItem('from-right');
-}
-
-document.querySelector('.left-arrow').addEventListener('click', function() {
-    if(isEnabled) {
-        previousItem(currentItem);
-    }
-})
-
-document.querySelector('.right-arrow').addEventListener('click', function() {
-    if(isEnabled) {
-        nextItem(currentItem);
-    }
-})
